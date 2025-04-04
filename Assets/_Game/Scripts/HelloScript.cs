aa2d9d50c7c912f8c4dbd38429a36ddebe505ffb
MizAless <44200635+MizAless@users.noreply.github.com>
2025-04-04 23:03:09 +0300

    Update HelloScript.cs

--- Assets/_Game/Scripts/HelloScript.cs
+++ Assets/_Game/Scripts/HelloScript.cs
@@ -10,31 +10,57 @@ public class HelloScript : MonoBehaviour
 
     private Vector3 _startScale;
 
+    private bool _done = false;
+
     private void Start()
     {
         _startScale = _text.transform.localScale;
         StartCoroutine(Hello());
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+            _done = true;
+    }
+
     private IEnumerator Hello()
     {
-        SetText("This game was made in 48 hours for Ludum Dare...", 3f);
-        yield return new WaitForSeconds(3f);
-
-        SetText("... by Mizaless", 2f);
-        yield return new WaitForSeconds(2f);
+        yield return SetText("This game was made in 48 hours for Ludum Dare...", 3f);
+        yield return null;
+        yield return SetText("... by Mizaless", 2f);
 
         SceneManager.LoadScene("MainScene");
     }
 
-    private void SetText(string text, float fadeDuration)
+
+    private IEnumerator SetText(string text, float fadeDuration)
     {
         _text.color = Color.white;
         _text.text = text;
         _text.transform.localScale = Vector3.zero;
         _text.transform.DOScale(_startScale, 0.2f);
         _text.DOFade(0f, fadeDuration);
-        StartCoroutine(HideTextWithDelay(fadeDuration, 0.5f));
+
+        yield return SetTimer(fadeDuration);
+        _text.DOKill();
+        _text.transform.DOScale(Vector3.zero, 0.5f);
+        yield return new WaitForSeconds(0.5f);
+    }
+
+    private IEnumerator SetTimer(float duration)
+    {
+        _done = false;
+        
+        float elapsedTime = 0f;
+
+        while (!_done)
+        {
+            elapsedTime += Time.deltaTime;
+            _done = elapsedTime > duration;
+
+            yield return null;
+        }
     }
 
     private IEnumerator HideTextWithDelay(float delay, float fadeDuration)
